2941ef512abd14003c6981c8c1540499fe0180c1
Zhykun22 <68529682+ZhyKun0322@users.noreply.github.com>
2025-12-22 09:53:40 +0800

    Update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -1,1 +1,11 @@
+// utils/logger.js
+const fs = require('fs');
 
+function log(msg) {
+  const time = new Date().toISOString();
+  const fullMsg = `[${time}] ${msg}`;
+  console.log(fullMsg);
+  fs.appendFileSync('logs.txt', fullMsg + '\n');
+}
+
+module.exports = { log };
