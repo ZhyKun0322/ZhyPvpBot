dac1f0e2820033d0c94d44d1c7353f8ec6c0c674
Zhykun22 <68529682+ZhyKun0322@users.noreply.github.com>
2025-12-22 11:42:17 +0800

    Update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -1,11 +1,11 @@
-// utils/logger.js  
-const fs = require('fs');  
-  
-function log(msg) {  
-  const time = new Date().toISOString();  
-  const fullMsg = `[${time}] ${msg}`;  
-  console.log(fullMsg);  
-  fs.appendFileSync('logs.txt', fullMsg + '\n');  
-}  
-  
-module.exports = { log };
+// utils/logger.js
+const fs = require('fs');
+
+function log(msg) {
+  const time = new Date().toISOString();
+  const fullMsg = `[${time}] ${msg}`;
+  console.log(fullMsg);
+  fs.appendFileSync('logs.txt', fullMsg + '\n');
+}
+
+module.exports = log; // export the function directly
